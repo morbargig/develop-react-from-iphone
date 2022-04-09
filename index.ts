9982e9f61be9d94aff5a45df462d0910634e276d
Mor Bargig <morbargig@gmail.com>
2022-04-09 15:04:43 +0300

    try run typescript on mobile

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import webpack, { Configuration } from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
+const express =require('express')
+const webpack =require('webpack')
+const webpackMiddleware =require('webpack-dev-middleware')
 
 // Setup
 const app = express();
 const port = process.env['REACT_APP_PORT'] || process.env['PORT'] || 80;
-import config from './webpack.config.js';
-const compiler = webpack(<Configuration>config);
+const config =require('./webpack.config.js')
+const compiler = webpack(config);
 const middleware = webpackMiddleware(compiler, {
   publicPath: config.output.publicPath,
   serverSideRender: false,
@@ -31,4 +31,4 @@ app.listen(port, () => {
 // Register app and middleware. Required for better
 // performance when running from play.js
 // @ts-ignore
-try { (pjs as any).register(app, middleware); } catch (error) { }
+try { (pjs).register(app, middleware); } catch (error) { }
