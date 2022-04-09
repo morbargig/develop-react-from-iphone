f27debdbecc4c1f4677a4ace6fe375aff913a5f6
mor bargig <mor@ideo-digital.com>
2022-04-09 19:42:23 +0300

    Auto commit by mor bargig at 2022-04-09T19:42+0300

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
-import logo from '../public/logo.png';
+import logo from './assets/logo.png';
 class App extends React.Component {
   render() {
     return (
