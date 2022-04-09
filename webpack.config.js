5e3f86bef2a3985ec220dc5d3685b45db6349c7a
mor bargig <mor@ideo-digital.com>
2022-04-09 18:13:26 +0300

    Auto commit by mor bargig at 2022-04-09T18:13+0300

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,29 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: [
-      './src/app.tsx'
+    server: [
+      './index.ts'
     ]
   },
+  // target: 'node',
+  // resolve: {
+  //   // extensions: ['.ts', '.js'],
+  // },
+  target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
-        test: (m) => { return /\.(ts|js|tsx|jsx)$/.test(m) },
-        exclude: (m) => { return /node_modules/.test(m) },
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react'
-            ]
-          }
-        }
-      },
-      {
-        test: (m) => { return /\.css$/.test(m) },
-        exclude: (m) => { return /node_modules/.test(m) },
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: (m) => { return /\.ts$/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) && /client/.test(m) },
+        use: ['ts-loader']
       },
       {
         test: (m) => { return /\.(png|jp(e*)g|svg)$/.test(m) },
-        exclude: (m) => { return /node_modules/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) && /client/.test(m) },
         use: [{
           loader: 'url-loader',
           options: {
@@ -46,7 +37,13 @@ module.exports = {
   plugins: [],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname),
     publicPath: '/'
+  },
+  node: {
+    __dirname: false
   }
+  // externals: {
+  //   express: 'express',
+  // },
 };
