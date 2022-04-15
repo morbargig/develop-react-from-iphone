54e31c772206cc47c70dd1e4cf573bc6727140e2
mor bargig <mor@ideo-digital.com>
2022-04-15 03:36:30 +0300

    Auto commit by mor bargig at 2022-04-15T03:36+0300

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -267,7 +267,6 @@ const styles: { [k: string]: React.CSSProperties } = {
   },
 };
 
-
 i18next.init({
   interpolation: { escapeValue: false },  // React already does escaping
   lng: 'en',                              // language to use
